78e4f0fdf19b970bfd4be19c9cd1cf9ea43d04e8
lombrix <206374544+l0mbrix@users.noreply.github.com>
2026-06-14 12:45:34 +0200

    Polish: sentence/gif 2

--- PingU.py
+++ PingU.py
@@ -34,7 +34,8 @@ async def hhbfest(interaction: discord.Interaction):
     today = datetime.date.today()
     difference = (event - today).days
     days = difference if difference >= 0 else 0
-    await interaction.response.send_message(f"Plus que {days} dodos avant le HHB Fest !")
+    await interaction.response.defer()
+    await interaction.followup.send(f"Plus que {days} dodos avant le HHB Fest !")
     await interaction.followup.send(gif_chosen)
 
 @bot.tree.command(name="pingvalo", description="Ping les joueurs de Valorant")
